b7509b4a1f1e67cd8620adc18ffc85a83167f2c8
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2022-04-04 16:31:34 +0300

    Рефакторинг

--- main/my_functions.h
+++ main/my_functions.h
@@ -36,39 +36,4 @@ namespace ntp_helper {
   }
 } // ntp_helper
 
-//void read_write_example() {
-//  // open the file. note that only one file can be open at a time,
-//  // so you have to close this one before opening another.
-//  myFile = SD.open("test.txt", FILE_WRITE);
-//
-//  // if the file opened okay, write to it:
-//  if (myFile) {
-//    Serial.print("Writing to test.txt...");
-//    myFile.println("testing 1, 2, 3.");
-//    // close the file:
-//    myFile.close();
-//    Serial.println("done.");
-//  } else {
-//    // if the file didn't open, print an error:
-//    Serial.println("error opening test.txt");
-//  }
-//
-//  // re-open the file for reading:
-//  myFile = SD.open("test.txt");
-//  if (myFile) {
-//    Serial.println("test.txt:");
-//
-//    // read from the file until there's nothing else in it:
-//    while (myFile.available()) {
-//      Serial.write(myFile.read());
-//    }
-//    // close the file:
-//    myFile.close();
-//  } else {
-//    // if the file didn't open, print an error:
-//    Serial.println("error opening test.txt");
-//  }
-//}
-
-
 #endif // MY_FUNCTIONS_H
